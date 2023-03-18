ff75c18de83032f932729b1be2ef91d3a37208e3
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-03-18 23:29:47 +0530

    magma

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const main = () => {
+  return (
+    <div className='bg-blue-200 h-screen w-full mt-[4px]'>
+
+    </div>
+  )
+}
+
+export default main
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BsThreeDots} from 'react-icons/bs'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-200 w-full h-12'>
+        <div className='pl-6 '><BsThreeDots size={50} color="white"/></div>
+    </div>
+  )
+}
+
+export default Navbar
